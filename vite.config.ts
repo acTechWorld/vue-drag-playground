002419b9255349d99c3f4b3dc5bef1f9d71b29b3
oncekiller <canard.antoine@gmail.com>
2024-11-24 20:40:19 +0100

    add config npm package

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), cssInjectedByJsPlugin()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, 'src'), // Alias '@' to your src directory
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'), // Adjust path to index.js if needed
+      name: 'VueDragPlayground',
+      fileName: (format) => `vue-drag-playground.${format}.js`,
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      // Ensure external dependencies are not bundled into your library
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
     },
   },
 })
