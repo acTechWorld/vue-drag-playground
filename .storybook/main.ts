c118e62ae136cdb17e15fbf0346077c1e6e60c9f
oncekiller <canard.antoine@gmail.com>
2024-11-30 17:22:54 +0100

    update images

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/vue3-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  staticDirs: ['../src/assets/images'],
+  staticDirs: ['../src/assets'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-essentials',
