a6051e83fbdd8e8d103833558e7d9e8c6278ab63
oncekiller <canard.antoine@gmail.com>
2024-12-14 12:26:20 +0100

    update index.d.ts for emits and v-model

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-import { App, DefineComponent } from 'vue'
+import { App, DefineComponent, EmitsOptions } from 'vue'
 
 // Define prop types
 export declare type DraggableItem = {
@@ -13,6 +13,7 @@ export declare type DraggableItem = {
 }
 
 export declare type DrapPlaygroundProps = {
+  modelValue: DraggableItem[] // v-model binds to 'modelValue'
   isDrag?: boolean
   isResize?: boolean
   isRotate?: boolean
@@ -24,9 +25,32 @@ export declare type DrapPlaygroundProps = {
   multiRotationMode?: 'proportional' | 'uniform' // "proportional" multirotation keep the based angle of item / "proportional" multirotation align items at the same angle"
 }
 
-// Declare the Vue component itself
-declare const VueDragPlayground: DefineComponent<DrapPlaygroundProps>
+// Define emits for custom events and v-model
+export declare type DrapPlaygroundEmits = {
+  // v-model event: update the 'items' array
+  'update:modelValue': (value: DraggableItem[]) => void // v-model emit
+  'drag-start': (item: DraggableItem) => void
+  dragging: (item: DraggableItem) => void
+  'drag-end': (item: DraggableItem) => void
+  'resize-start': (item: DraggableItem) => void
+  resizing: (item: DraggableItem) => void
+  'resize-end': (item: DraggableItem) => void
+  'rotate-start': (item: DraggableItem) => void
+  rotating: (item: DraggableItem) => void
+  'rotate-end': (item: DraggableItem) => void
+}
 
+// Declare the Vue component itself
+declare const VueDragPlayground: DefineComponent<
+  DrapPlaygroundProps,
+  object,
+  object,
+  Record<string, never>,
+  Record<string, never>,
+  object,
+  object,
+  DrapPlaygroundEmits
+>
 // Declare the install function for the plugin system
 declare const _default: {
   install(app: App): void
